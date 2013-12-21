ed6fc5e69e9dbb41f3cc12d8f7c58f91aafe03c1
Lindsey Kuper <lindsey@composition.al>
2013-12-20 23:34:35 -0500

    The MaxCounter example seems to be working now.

--- repeated-write-lvar-max-counter.hs
+++ repeated-write-lvar-max-counter.hs
@@ -1,11 +1,10 @@
--- Warning: I don't work yet!
-
 -- This program does two writes to an LVar that are resolved with
--- `max`.
+-- `max`.  Although `waitThresh` only returns the unit value `()`, the
+-- fact that it returns at all means that the LVar has reached 4.
 
 {-# LANGUAGE DataKinds #-}
 
-import Control.LVish  -- Generic scheduler; works with any lattice.
+import Control.LVish hiding (put) -- Generic scheduler; works with any lattice.
 import Data.LVar.MaxCounter -- The particular lattice in question.
 
 p :: Par Det s ()

370862fa8a299b8fcc27bac5df0146e21f703323
Lindsey Kuper <lindsey@composition.al>
2014-01-19 22:41:44 -0700

    Do map-lvar-getkey.hs in a less silly way.

--- map-lvar-getkey.hs
+++ map-lvar-getkey.hs
@@ -18,94 +18,73 @@ import Control.LVish
 import Control.LVish.Internal (liftIO)
 import Data.LVar.Internal.Pure
 
-data Item = Book | Shoes
-  deriving (Show, Ord, Eq)
-
 -- To keep it finite, we're assuming that two is the most you can get
 -- of any item.  Bot is like a quantity of zero for everything.
 data CartState = Bot
-               | BookOne
-               | BookTwo
-               | ShoesOne
-               | ShoesTwo
-               | BookOneShoesOne
-               | BookOneShoesTwo
-               | BookTwoShoesOne
-               | BookTwoShoesTwo
+               | Book Count
+               | Shoes Count
+               | BookShoes Count Count
                | Top
-  deriving (Show, Ord, Eq, Enum)
+  deriving (Show, Ord, Eq)
+
+data Count = One | Two
+  deriving (Show, Ord, Eq)
 
 instance JoinSemiLattice CartState
          where join = joinCartStates
 
--- This is very, very silly.
 joinCartStates :: CartState -> CartState -> CartState
 joinCartStates x y | x == y = x
 joinCartStates Bot x = x
 joinCartStates Top _ = Top
-joinCartStates _ Top = Top
-
-joinCartStates BookOne ShoesOne = BookOneShoesOne
-joinCartStates BookOne ShoesTwo = BookOneShoesTwo
-joinCartStates BookTwo ShoesOne = BookTwoShoesOne
-joinCartStates BookTwo ShoesTwo = BookTwoShoesTwo
-
-joinCartStates BookOne BookTwo = Top
-joinCartStates ShoesOne ShoesTwo = Top
-
-joinCartStates BookOneShoesOne BookOne = BookOneShoesOne
-joinCartStates BookOneShoesOne ShoesOne = BookOneShoesOne
-joinCartStates BookOneShoesTwo BookOne = BookOneShoesTwo
-joinCartStates BookOneShoesTwo ShoesTwo = BookOneShoesTwo
-joinCartStates BookTwoShoesOne BookTwo = BookTwoShoesOne
-joinCartStates BookTwoShoesOne ShoesOne = BookTwoShoesOne
-joinCartStates BookTwoShoesTwo BookTwo = BookTwoShoesTwo
-joinCartStates BookTwoShoesTwo ShoesTwo = BookTwoShoesTwo
-
-joinCartStates BookOneShoesOne BookTwo = Top
-joinCartStates BookOneShoesOne ShoesTwo = Top
-joinCartStates BookOneShoesTwo BookTwo = Top
-joinCartStates BookOneShoesTwo ShoesOne = Top
-joinCartStates BookTwoShoesOne BookOne = Top
-joinCartStates BookTwoShoesOne ShoesTwo = Top
-joinCartStates BookTwoShoesTwo BookOne = Top
-joinCartStates BookTwoShoesTwo ShoesOne = Top
-
-joinCartStates BookOneShoesOne BookOneShoesTwo = Top
-joinCartStates BookOneShoesOne BookTwoShoesOne = Top
-joinCartStates BookOneShoesOne BookTwoShoesTwo = Top
-
-joinCartStates BookOneShoesTwo BookTwoShoesOne = Top
-joinCartStates BookOneShoesTwo BookTwoShoesTwo = Top
-
-joinCartStates BookTwoShoesOne BookOneShoesTwo = Top
-joinCartStates BookTwoShoesOne BookTwoShoesTwo = Top
-
+joinCartStates (Book x) (Book y) =
+  if x == y then Book x else Top
+joinCartStates (Shoes x) (Shoes y) =
+  if x == y then Shoes x else Top
+joinCartStates (Book x) (Shoes y) = BookShoes x y
+joinCartStates (BookShoes x1 x2) (BookShoes y1 y2) =
+  if (x1 == y1) && (x2 == y2) then (BookShoes x1 x2) else Top
+joinCartStates (Book x1) (BookShoes x2 y2) =
+  if (x1 == x2) then (BookShoes x2 y2) else Top
+joinCartStates (Shoes y1) (BookShoes x2 y2) =
+  if (y1 == y2) then (BookShoes x2 y2) else Top
 joinCartStates x y = joinCartStates y x
+
+getBookCount :: PureLVar s CartState -> Par Det s CartState
+getBookCount cart = getPureLVar cart [Book One, Book Two]
 
 p :: Par Det s CartState
 p = do
   cart <- newPureLVar Bot
   fork $ do --liftIO $ threadDelay 10000;
-            putPureLVar cart BookOne
+            putPureLVar cart (Book One)
   fork $ do --liftIO $ threadDelay 10000;
-            putPureLVar cart ShoesTwo
+            putPureLVar cart (Shoes Two)
   -- This threshold set says, "Tell me how many books are in the
   -- cart."  It will return BookOne, regardless of whether ShoesTwo is
-  -- written yet or not (the actual contents will be either BookOne or
-  -- BookOneShoesTwo at the time it unblocks).
-  getPureLVar cart [BookOne, BookTwo]
+  -- written yet or not (the actual contents will be either (Book One)
+  -- or (BookShoes One Two) at the time it unblocks).
+  getBookCount cart
 
 main = do
   -- printAllJoins
   putStr $ show $ runPar p
 
--- This helped me get joinCartStates right.
 printAllJoins = do
   putStrLn $ showStrings
     ["join " ++ show x ++ " " ++ show y ++ " = " ++ show (joinCartStates x y)
-    | x <- [Bot .. Top],
-      y <- [Bot .. Top]]
+    | x <- allStates,
+      y <- allStates]
   where showStrings strings = case strings of
           [] -> ""
           (x : xs) -> show x ++ "\n" ++ showStrings xs
+        allStates = [Bot,
+                     Book One,
+                     Book Two,
+                     Shoes One,
+                     Shoes Two,
+                     BookShoes One One,
+                     BookShoes One Two,
+                     BookShoes Two One,
+                     BookShoes Two Two,
+                     Top]

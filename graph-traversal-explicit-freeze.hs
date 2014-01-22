5ce94d3c0f01b178da2d76b57eec594e4db91f50
Lindsey Kuper <lindsey@composition.al>
2014-01-22 15:48:14 -0800

    Do the first insertion after addHandler for now.

--- graph-traversal-explicit-freeze.hs
+++ graph-traversal-explicit-freeze.hs
@@ -35,12 +35,12 @@ neighbors g v =
 traverse :: G.Graph -> Int -> Par QuasiDet s (ISet Frzn Int)
 traverse g startNode = do
   seen <- newEmptySet
-  insert startNode seen -- Kick things off
   h <- newHandler seen
        (\node -> do
            mapM (\v -> insert v seen)
              (neighbors g node)
            return ())
+  insert startNode seen -- Kick things off
   quiesce h
   freeze seen
 

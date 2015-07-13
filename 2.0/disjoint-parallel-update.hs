4ddaea7905102b2620e24ae471c1ba9a30fe6f5d
Lindsey Kuper <lindsey@composition.al>
2015-07-12 21:12:15 -0700

    Tweaks to disjoint-parallel-update example.

--- 2.0/disjoint-parallel-update.hs
+++ 2.0/disjoint-parallel-update.hs
@@ -1,18 +1,26 @@
 {-# LANGUAGE TypeFamilies #-}
 
 import Control.LVish
-import qualified Control.Par.ST             as PST
-import qualified Control.Par.ST.Vec         as V
-import           Control.Monad              (void)
-import           Data.Vector                (freeze, toList)
+import qualified Control.Par.ST as PST
+import qualified Control.Par.ST.Vec as V
+import Control.Monad (void)
+import Data.Vector (freeze, toList)
 
-p :: (HasGet e, HasPut e) => PST.ParST (PST.MVectorFlp Int s1) Par e s [Int]
+p :: (HasGet e, HasPut e) => PST.ParST (PST.MVectorFlp String s1) Par e s [String]
 p = do
-  V.set 0
-  void (V.forkSTSplit 5 (V.write 0 5) (V.write 0 120))
-  raw <- V.reify
-  frozen <- PST.liftST (freeze raw)
-  return (toList frozen)           
+  -- Fill all six ten slots in the vector with "foo".
+  V.set "foo"
+  -- Get a pointer to the state.
+  ptr <- V.reify
 
-main = print (runPar (V.runParVecT 10 p))
+  -- Fork two computations, each of which has access to half the
+  -- vector.  Within the two forked child computations, `ptr` is
+  -- inaccessible.
+  V.forkSTSplit 3 -- Split at index 3 in the vector.
+                (V.write 0 "bar")
+                (V.write 0 "baz")
 
+  frozen <- PST.liftST (freeze ptr)
+  return (toList frozen)
+
+main = print (runPar (V.runParVecT 6 p))
